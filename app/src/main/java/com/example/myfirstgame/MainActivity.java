d56892258766d2c04b35fd21936c714b3e25530b
creativeBubble <dennis_doerries@yahoo.de>
2020-12-14 22:06:37 +0100

    Gameloop

--- app/src/main/java/com/example/myfirstgame/MainActivity.java
+++ app/src/main/java/com/example/myfirstgame/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myfirstgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    /*
+    Entry point
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //ss
+
+        //Window is now fullscreen (no status bar anymore)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      //Sets App Layout. Shows Game
+        setContentView(new Game(this));
     }
 }
